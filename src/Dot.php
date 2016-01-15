44b8ae9f8df8f8cbc039f6b8eacf3c081ab3f860
Danila Ridzhi <danilaridzhi@gmail.com>
2016-01-15 17:18:46 +0300

    init commit

---
+++ src/Dot.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Arress;
+
+
+class Dot
+{
+
+}
---
+++ tests/DotTest.php
@@ -0,0 +1,12 @@
+<?php
+
+
+class DotTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testInit()
+    {
+        $this->assertEquals(1, 1);
+    }
+
+}
