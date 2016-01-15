5ec21fd9d2457e9fe9057b20f863f3a0f2bf9e21
Danila Ridzhi <danilaridzhi@gmail.com>
2016-01-15 18:57:50 +0300

    add get method

--- src/Dot.php
+++ src/Dot.php
@@ -6,4 +6,53 @@ namespace Arress;
 class Dot
 {
 
+    const SEPARATOR = '.';
+
+    protected $data;
+
+    private function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    public static function with(array &$data)
+    {
+        return new self($data);
+    }
+
+    public function get($key, $default = null)
+    {
+        $segments = explode(self::SEPARATOR, $key);
+        $current = $this->data;
+
+        $length = count($segments);
+        while ($length-- > 0) {
+            $k = array_shift($segments);
+
+            if (is_array($current) && array_key_exists($k, $current)) {
+                $current = $current[$k];
+            } else {
+                return $default;
+            }
+        }
+
+        return $current;
+    }
+
+
+    public function set($key, $value)
+    {
+
+    }
+
+    public function has($key)
+    {
+
+    }
+
+    public function remove()
+    {
+
+    }
+
 }
--- tests/DotTest.php
+++ tests/DotTest.php
@@ -1,12 +1,28 @@
 <?php
 
 
+use Arress\Dot;
+
 class DotTest extends PHPUnit_Framework_TestCase
 {
+
+    protected $data;
+
+
+    public function setUp()
+    {
+
+    }
+
 
     public function testInit()
     {
         $this->assertEquals(1, 1);
+    }
+
+    public function testGet()
+    {
+
     }
 
 }
