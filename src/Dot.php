f2bc9ed9f740529eb9357de5ec71f3f29c0361ad
Danila Ridzhi <danilaridzhi@gmail.com>
2016-01-17 23:05:56 +0300

    make set/get + tests

--- src/Dot.php
+++ src/Dot.php
@@ -9,22 +9,22 @@ class Dot
 
     protected $data;
 
-    private function __construct(array $data)
+    private function __construct(array & $data)
     {
-        $this->data = $data;
+        $this->data = &$data;
     }
 
-    public static function with(array &$data)
+    public static function with(array & $data)
     {
         return new self($data);
     }
 
     public function get($key, $default = null)
     {
-        $segments = explode(self::SEPARATOR, $key);
+        $segments = self::parseKey($key);
         $current = $this->data;
-
         $length = count($segments);
+
         while ($length-- > 0) {
             $k = array_shift($segments);
 
@@ -38,10 +38,31 @@ class Dot
         return $current;
     }
 
-
     public function set($key, $value)
     {
+        $segments = self::parseKey($key);
+        $current = &$this->data;
+        $length = count($segments);
 
+        $i = 0;
+        while ($length-- > 0) {
+            $k = $segments[$i++];
+
+            if (is_array($current)) {
+
+                if (!array_key_exists($k, $current)) {
+                    $current[$k] = [];
+                }
+
+                $current = &$current[$k];
+            } else {
+                $scalarKey = implode('.', array_slice($segments, 0, --$i));
+                throw new \InvalidArgumentException(sprintf('invalid key, %s is scalar, expected array', $scalarKey));
+            }
+
+        }
+
+        $current = $value;
     }
 
     public function has($key)
@@ -52,6 +73,15 @@ class Dot
     public function remove()
     {
 
+    }
+
+    protected static function parseKey($key)
+    {
+        if ($key === '') {
+            return [];
+        }
+
+        return explode(self::SEPARATOR, $key);
     }
 
 }
--- tests/DotTest.php
+++ tests/DotTest.php
@@ -5,23 +5,131 @@ use Smarrt\Dot;
 class DotTest extends PHPUnit_Framework_TestCase
 {
 
+    /**
+     * @var ReflectionClass
+     */
+    protected static $inst;
+
+    /**
+     * @var Dot
+     */
+    protected $obj;
+
     protected $data;
 
-
-    public function setUp()
+    public static function setUpBeforeClass()
     {
-
+        self::$inst = new ReflectionClass('\Smarrt\Dot');
     }
 
-
-    public function testInit()
+    protected function setUp()
     {
-        $this->assertEquals(1, 1);
+        $this->data = [
+            'users' => [
+                'admin' => [
+                    'user_1',
+                    'user_2',
+                    'root' => [
+                        'root_1',
+                        'root_2'
+                    ]
+                ]
+            ],
+            [
+                ['Sergey', 'Alexander'],
+                ['Natalya', 'Katerina']
+            ]
+        ];
+
+        $this->obj = Dot::with($this->data);
     }
 
-    public function testGet()
+    public function testGetAssocKey()
     {
+        $actual = $this->obj->get('users.admin.root');
+        $this->assertEquals($this->data['users']['admin']['root'], $actual);
+    }
 
+    public function testGetIndexKey()
+    {
+        $actual = $this->obj->get('0.1.1');
+        $this->assertEquals($this->data[0][1][1], $actual);
+    }
+
+    public function testGetAssocIndexKey()
+    {
+        $actual = $this->obj->get('users.admin.1');
+        $this->assertEquals($this->data['users']['admin'][1], $actual);
+    }
+
+    public function testGetDefault()
+    {
+        $actual = $this->obj->get('users.some_not_exists', 'default');
+        $this->assertEquals('default', $actual);
+    }
+
+    public function testSetAssocKey()
+    {
+        $expected = $this->data;
+        $this->obj->set('users.admin.root', 'root_upd');
+        $expected['users']['admin']['root'] = 'root_upd';
+        $this->assertEquals($expected, $this->data);
+    }
+
+    public function testSetIndexKey()
+    {
+        $expected = $this->data;
+        $this->obj->set('0.1.1', 'Katerina_upd');
+        $expected[0][1][1] = 'Katerina_upd';
+        $this->assertEquals($expected, $this->data);
+    }
+
+    public function testSetAssocIndexKey()
+    {
+        $expected = $this->data;
+        $this->obj->set('users.admin.root.1', 'root_upd');
+        $expected['users']['admin']['root'][1] = 'root_upd';
+        $this->assertEquals($expected, $this->data);
+    }
+
+    public function testSetIfKeyNotExists()
+    {
+        $expected = $this->data;
+        $this->obj->set('users.moderator.male.2', 'moder');
+        $expected['users']['moderator'] = [];
+        $expected['users']['moderator']['male'] = [];
+        $expected['users']['moderator']['male'][2] = 'moder';
+        $this->assertEquals($expected, $this->data);
+    }
+
+    public function testSetIfKeyNotExistsForEmpty()
+    {
+        $expected = $actual = [];
+        Dot::with($actual)->set('users.moderator', 'moder');
+        $expected['users'] = [];
+        $expected['users']['moderator'] = 'moder';
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @dataProvider parseKeyProvider
+     */
+    public function testParseKey($key, $expected)
+    {
+        $method = self::$inst->getMethod('parseKey');
+        $method->setAccessible(true);
+        $actual = $method->invoke(null, $key);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function parseKeyProvider()
+    {
+        return [
+            'empty' => ['', []],
+            'one' => ['users', ['users']],
+            'few' => ['users.admin.root', ['users', 'admin', 'root']],
+            'withIndex' => ['users.0.admin.0.1', ['users', '0', 'admin', '0', '1']]
+        ];
     }
 
 }
